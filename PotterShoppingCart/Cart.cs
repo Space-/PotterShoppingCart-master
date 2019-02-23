14e893e195bb0ad240b97345366229357fb80db8
bruce.kao38 <bruce.kao38@TI03NB134>
2019-02-23 11:33:55 +0800

    Add project files.

---
+++ PotterShoppingCart/Cart.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Linq;
+
+namespace PotterShoppingCart
+{
+    public class Cart
+    {
+        private int _basicPrize = 100;
+
+        private Dictionary<int, double> _discount = new Dictionary<int, double>
+        {
+            {0, 0}, {1, 1}, {2, 0.95},{3, 0.9},{4, 0.8},{5, 0.75}
+        };
+
+        public int GetTotalPrize(List<Book> books)
+        {
+            if (!books.Any())
+            {
+                return 0;
+            }
+            var suites = books.GroupBy(x => x.ISBN).Select(x => new { Episode = x.Key, BookCount = x.Count() });
+
+            int totalPrize = 0;
+            for (var i = 1; i <= suites.Max(x => x.BookCount); i++)
+            {
+                totalPrize += GetSuitePrize(suites.Where(x => x.BookCount >= i).Select(x=>x.BookCount).ToList());
+            }
+
+            return totalPrize;
+        }
+
+        private int GetSuitePrize(List<int> books)
+        {
+            var totalPrize = _basicPrize * books.Count;
+            return (int)(totalPrize * _discount[books.Count]);
+        }
+    }
+}
---
+++ PotterShoppingCart/CartTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PotterShoppingCart
+{
+    [TestClass]
+    public class CartTests
+    {
+        [TestMethod]
+        public void GetTotalPrize_Buy_Nothing_Should_return_0()
+        {
+            var books = new List<Book>();
+            TotalPrizeShouldBe(0, books);
+        }
+
+        [TestMethod]
+        public void GetTotalPrize_Buy_One_EP1_Should_return_100()
+        {
+            var books = new List<Book> { new Book() { ISBN = "1" } };
+            TotalPrizeShouldBe(100, books);
+        }
+
+        [TestMethod]
+        public void GetTotalPrize_Buy_Two_Different_Episode_Should_return_190()
+        {
+            var books = new List<Book> { new Book() { ISBN = "1" }, new Book() { ISBN = "2" } };
+            TotalPrizeShouldBe(190, books);
+        }
+
+        [TestMethod]
+        public void GetTotalPrize_Buy_Three_Different_Episode_Should_return_270()
+        {
+            var books = new List<Book>
+            {
+                new Book() { ISBN = "1" },
+                new Book() { ISBN = "2" },
+                new Book() { ISBN = "3" },
+            };
+            TotalPrizeShouldBe(270, books);
+        }
+
+        [TestMethod]
+        public void GetTotalPrize_Buy_Four_Different_Episode_Should_return_320()
+        {
+            var books = new List<Book>
+            {
+                new Book() { ISBN = "1" },
+                new Book() { ISBN = "2" },
+                new Book() { ISBN = "3" },
+                new Book() { ISBN = "4" },
+            };
+            TotalPrizeShouldBe(320, books);
+        }
+
+        [TestMethod]
+        public void GetTotalPrize_Buy_Five_Different_Episode_Should_return_375()
+        {
+            var books = new List<Book>
+            {
+                new Book() { ISBN = "1" },
+                new Book() { ISBN = "2" },
+                new Book() { ISBN = "3" },
+                new Book() { ISBN = "4" },
+                new Book() { ISBN = "5" },
+            };
+            TotalPrizeShouldBe(375, books);
+        }
+        [TestMethod]
+        public void GetTotalPrize_Buy_two_Ep1_and_Ep2_Should_return_290()
+        {
+            var books = new List<Book>
+            {
+                new Book() { ISBN = "1" },
+                new Book() { ISBN = "2" },
+                new Book() { ISBN = "1" },
+            };
+            TotalPrizeShouldBe(290, books);
+        }
+        private static void TotalPrizeShouldBe(int expected, List<Book> books)
+        {
+            var cart = new Cart();
+            var actual = cart.GetTotalPrize(books);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
